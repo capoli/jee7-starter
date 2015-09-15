5c8091f911be0ee0c5448c1e55519fbaab993100
unknown <OCPAX79@WP2041.ATOMIUM.local>
2015-09-15 10:17:09 +0200

    MessageDrivenBean for ticket + test

--- ejb-module/src/main/java/com/realdolmen/course/consumer/TextMessageConsumer.java
+++ ejb-module/src/main/java/com/realdolmen/course/consumer/TextMessageConsumer.java
@@ -1,26 +1,67 @@
 package com.realdolmen.course.consumer;
 
 
+import com.realdolmen.course.domain.Flight;
+import com.realdolmen.course.domain.Passenger;
+import com.realdolmen.course.domain.Ticket;
+
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @MessageDriven(activationConfig = {
         @ActivationConfigProperty(propertyName = "destination", propertyValue = "java:jboss/exported/jms/queue/MyQueue"),
 })
 public class TextMessageConsumer implements MessageListener {
+    @PersistenceContext
+    private EntityManager em;
+
     @Override
     public void onMessage(Message message) {
         try {
-            TextMessage textMessage = (TextMessage) message;
+            /*TextMessage textMessage = (TextMessage) message;
             System.out.println("**********************");
             System.out.println(textMessage.getText());
-            System.out.println("**********************");
+            System.out.println("**********************");*/
+            TextMessage textMessage = (TextMessage) message;
+            for(String line : textMessage.getText().split("\n")) {
+                String[] strings = line.split(",");
+                System.out.println("**********************");
+                if(strings[0].equalsIgnoreCase("null")) {
+                    createTicket(
+                            Double.parseDouble(strings[1]),
+                            Long.parseLong(strings[3]),
+                            Long.parseLong(strings[4]));
+                }
+                else {
+                    updateTicket(
+                            Long.parseLong(strings[0]),
+                            Double.parseDouble(strings[1]));
+                }
+                System.out.println("**********************");
+            }
         } catch (JMSException e) {
             throw new RuntimeException("Deal with this", e);
         }
+    }
+
+    private void createTicket(Double price, Long flightId, Long passengerId) {
+        Ticket ticket = new Ticket(price);
+        ticket.setFlight(em.find(Flight.class,flightId));
+        ticket.setPassenger(em.find(Passenger.class, passengerId));
+        em.persist(ticket);
+        System.out.println("Create ticket");
+    }
+
+    private void updateTicket(Long ticketId, Double price) {
+        Ticket ticket = em.find(Ticket.class, ticketId);
+        ticket.setPrice(price);
+        //em.persist(ticket);
+        System.out.println("Update ticket");
     }
 }
--- ejb-module/src/test/java/com/realdolmen/course/integration/TextMessageConsumerTest.java
+++ ejb-module/src/test/java/com/realdolmen/course/integration/TextMessageConsumerTest.java
@@ -5,6 +5,14 @@ import org.junit.Test;
 public class TextMessageConsumerTest extends RemoteJmsTest {
     @Test
     public void produceTextMessage() throws Exception {
-        producer.send(session.createTextMessage("Hello World!"));
+        //producer.send(session.createTextMessage("Hello World!"));
+        producer.send(session.createTextMessage(String.format(
+                "%d,%f\n%d,%f\nnull,%f,%d,%d\nnull,%f,%d,%d\nnull,%f,%d,%d\nnull,%f,%d,%d\n",
+                1000l, 30.0,
+                2000l, 40.0,
+                30.0, 1000l, 1000l,
+                40.0, 2000l, 2000l,
+                40.0, 1000l, 2000l,
+                35.0, 2000l, 1000l)));
     }
 }
